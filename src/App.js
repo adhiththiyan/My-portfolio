048b7056e5f22b808da676ae2ebb631c6d85c826
Adhiththiyan <137081577+adhiththiyan@users.noreply.github.com>
2024-05-20 15:55:39 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
-import { Route, Routes } from "react-router-dom";
-import About from "./routers/About";
-import Contact from "./routers/Contact";
-import Home from "./routers/Home";
-import Project from "./routers/Project";
-
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import About from './routers/About';
+import Contact from './routers/Contact';
+import Home from './routers/Home';
+import Project from './routers/Project';
 
 function App() {
   return (
     <div>
-
-<Routes>
-  <Route path="/" element={<Home/>}/>
-  <Route path="/project" element={<Project/>}/>
-  <Route path="/about" element={<About/>}/>
-  <Route path="/contact" element={<Contact/>}/>
-</Routes>
-
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/project" element={<Project />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
     </div>
   );
 }
