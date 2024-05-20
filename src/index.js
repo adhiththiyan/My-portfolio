446bbd85d77471fe03121af80398b8b18232bf13
Adhiththiyan <137081577+adhiththiyan@users.noreply.github.com>
2024-05-20 15:55:05 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,12 +4,9 @@ import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  // <BrowserRouter>
+  <BrowserRouter basename="/My-portfolio">
     <App />
-  // {/* </BrowserRouter> */}
+  </BrowserRouter>
 );
-
-
